0ad1bec8843504816ea816456798d7c968111f12
vthorlaksson <vthorlaksson@gmail.com>
2014-10-18 18:52:46 +0000

    If delimiter starts with "//"

--- src/main/java/is/ru/stringcalculator/calculator.java
+++ src/main/java/is/ru/stringcalculator/calculator.java
@@ -1,12 +1,17 @@
 package is.ru.stringcalculator;
 
+import java.util.ArrayList;
+import java.util.regex.*;
+
 public class Calculator {
 
-	public static int add (String text){
+	public static int add(String text){
 		if (text.equals("")){
 			return 0;
 		}
-		else if(text.contains(",")){
+
+		//delimiter getur verið "," eða new line
+		else if(text.contains(",") || text.contains("\n")){
 			return sum(splitNumbers(text));
 		}
 		else
@@ -17,8 +22,17 @@ public class Calculator {
 		return Integer.parseInt(number);
 	}
 
-	private static String[] splitNumbers(String numbers){
-		return numbers.split(",");
+	private static String[] splitNumbers(String number){
+		//athuga hvort delimiter byrjar á "//"
+		if(number.startsWith("//")){
+			Matcher m = Pattern.compile("//(.)\n(.*)").matcher(number);
+			m.matches();
+			String delimiter = m.group(1);
+			String restOfNumber = m.group(2);
+			return restOfNumber.split(delimiter);
+		}
+
+		return number.split(",|\n");
 	}
      
     private static int sum(String[] numbers){
@@ -26,6 +40,8 @@ public class Calculator {
 		for(String number : numbers){
 			total += toInt(number);
 		}
+
 		return total;
-    }
-}
+	}
+
+}	
--- src/test/java/is/ru/stringcalculator/calculatorTest.java
+++ src/test/java/is/ru/stringcalculator/calculatorTest.java
@@ -29,4 +29,16 @@ public class CalculatorTest {
 		assertEquals(15, Calculator.add("1,2,3,4,5"));
 	}
 
+	@Test
+	public void testNewLineNumbers(){
+		assertEquals(6, Calculator.add("1\n2,3"));
+	}
+
+	@Test
+	public void testNewDelimitter(){
+		assertEquals(3, Calculator.add("//;\n1;2"));
+	}
 }
+
+
+
